2daf8bd8632cc67700e353cbcef9ef8cc98d2889
Joen Asmussen <joen@automattic.com>
2014-11-17 10:08:48 +0100

    Improved JS for the demo page.

--- svg-set/gridicons-demo.js
+++ svg-set/gridicons-demo.js
@@ -1,52 +1,57 @@
 /**
  * Gridicons Demo page JS
  */
-
-function suggestCode ( attr, cssclass ) {
-
-	var cdn = '/i/gridicons/gridicons.svg'; // can't point to static domain as SVG CORS prevents that
-	var suggestion = '<svg class="gi"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="' + cdn + attr + '"></use></svg>';
-
-	window.prompt( "Copy this, paste in your HTML.\n\nNote: for now, remember to upload the gridicons.svg file as well!", suggestion );
-}
-
-jQuery(document).ready(function() {
-
-	// pick random icon if no permalink, otherwise go to permalink
-	/*if ( window.location.hash ) {
-		permalink = window.location.hash.split('#')[1];
-		attr = jQuery( '.gridicon-' + permalink ).find( 'use' ).attr( 'xlink:href' );
-		cssclass = permalink;
-		displayGlyph( attr, cssclass );
-	} else {
-		pickRandomIcon();
-	}*/
-
-	jQuery( '#icons svg' ).click(function() {
-	
-		attr = jQuery( this ).find('use').attr( 'xlink:href' );
-		cssclass = attr.split('#')[1];
-
-		suggestCode( attr, cssclass );
-
-	});
-
-	/*var $rows = jQuery('#icons svg');
-	jQuery('#search').keyup(function() {
-	
-		// remove update text when using search
-		jQuery('body').addClass('searching');
-	
-	    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-	
-	    $rows.show().filter(function() {
-	        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-	        return !~text.indexOf(val);
-	    }).hide();
-	});
-	
-	jQuery('input#search').focus();*/
-
-
-});
-
+(function( $ ){
+    // Kick things off
+    $( document ).ready( function() {
+        initEvents();
+        //initCheckHash();
+    });
+    function displayGlyph() {
+        //TODO: Add this functionality
+    }
+    function initCheckHash() {
+        // pick random icon if no permalink, otherwise go to permalink
+        if ( window.location.hash ) {
+            var permalink = window.location.hash.split('#')[1],
+                attr = $( '.gridicon-' + permalink ).find( 'use' ).attr( 'xlink:href' );
+             
+            displayGlyph( attr, permalink );
+        } else {
+            pickRandomIcon();
+        }
+    }
+    function initEvents() {
+        var $rows = $('#icons').find('svg');
+        // When you click on an icon
+        $rows.on( 'click', function() {
+            var attr = $( this ).find( 'use' ).attr( 'xlink:href' ),
+                cssclass = attr.split( '#' )[1];
+             
+            suggestCode( attr, cssclass );
+         
+        });
+        // Show results while searching
+        /*$( '#search' ).keyup( function() {
+            // remove update text when using search
+            $( 'body' ).addClass( 'searching' );
+             
+            var val = $.trim( $( this ).val() ).replace(/ +/g, ' ').toLowerCase();
+             
+            $rows.show().filter( function() {
+                var text = $( this ).text().replace(/\s+/g, ' ').toLowerCase();
+                return !~text.indexOf( val );
+            }).hide();
+        });*/
+    }
+    function pickRandomIcon() {
+        //TODO: Add this functionality
+    }
+    function suggestCode( attr, cssclass ) {
+	    // can't point to static domain as SVG CORS prevents that 
+        var cdn = '/i/gridicons/gridicons.svg',
+            suggestion = '<svg class="gi" width="24px" height="24px"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="' + cdn + attr + '"></use></svg>';
+         
+        window.prompt( "Copy this, paste in your HTML.\n\nNote: for now, remember to upload the gridicons.svg file as well!", suggestion );
+    }
+})( jQuery );
