a8c3e968f9fec03c0c43de58ff605ded33d646fa
mtias <mv@matiasventura.com>
2015-07-22 13:04:39 +0200

    Replace gridicon.jsx with folder component and design docs gallery.

---
+++ react/gridicon/design.jsx
@@ -0,0 +1,73 @@
+/**
+ * External dependencies
+ */
+var React = require( 'react' );
+
+/**
+ * Internal dependencies
+ */
+var Gridicon = require( 'components/gridicon' );
+
+module.exports = React.createClass( {
+	displayName: 'Gridicons',
+
+	render: function() {
+		return (
+			<div className="design-assets__group">
+				<h2>Gridicons</h2>
+				<Gridicon icon="aligncenter" size={ 48 } />
+				<Gridicon icon="alignleft" size={ 48 } />
+				<Gridicon icon="alignright" size={ 48 } />
+				<Gridicon icon="arrow" size={ 48 } />
+				<Gridicon icon="attachment" size={ 48 } />
+				<Gridicon icon="backspace" size={ 48 } />
+				<Gridicon icon="bell-dot" size={ 48 } />
+				<Gridicon icon="bell" size={ 48 } />
+				<Gridicon icon="block" size={ 48 } />
+				<Gridicon icon="bold" size={ 48 } />
+				<Gridicon icon="bookmark" size={ 48 } />
+				<Gridicon icon="bookmarked" size={ 48 } />
+				<Gridicon icon="calendar" size={ 48 } />
+				<Gridicon icon="camera" size={ 48 } />
+				<Gridicon icon="cell" size={ 48 } />
+				<Gridicon icon="checkmark" size={ 48 } />
+				<Gridicon icon="cloud-download" size={ 48 } />
+				<Gridicon icon="cloud-empty" size={ 48 } />
+				<Gridicon icon="cloud-upload" size={ 48 } />
+				<Gridicon icon="cloud" size={ 48 } />
+				<Gridicon icon="code" size={ 48 } />
+				<Gridicon icon="cross" size={ 48 } />
+				<Gridicon icon="dropdown" size={ 48 } />
+				<Gridicon icon="edit" size={ 48 } />
+				<Gridicon icon="ellipsis" size={ 48 } />
+				<Gridicon icon="flag" size={ 48 } />
+				<Gridicon icon="headline" size={ 48 } />
+				<Gridicon icon="help" size={ 48 } />
+				<Gridicon icon="italic" size={ 48 } />
+				<Gridicon icon="link-break" size={ 48 } />
+				<Gridicon icon="link" size={ 48 } />
+				<Gridicon icon="list-ordered" size={ 48 } />
+				<Gridicon icon="list-unordered" size={ 48 } />
+				<Gridicon icon="mail" size={ 48 } />
+				<Gridicon icon="menu" size={ 48 } />
+				<Gridicon icon="microphone" size={ 48 } />
+				<Gridicon icon="minus-small" size={ 48 } />
+				<Gridicon icon="minus" size={ 48 } />
+				<Gridicon icon="my-sites" size={ 48 } />
+				<Gridicon icon="note" size={ 48 } />
+				<Gridicon icon="plus-small" size={ 48 } />
+				<Gridicon icon="plus" size={ 48 } />
+				<Gridicon icon="quote" size={ 48 } />
+				<Gridicon icon="reader" size={ 48 } />
+				<Gridicon icon="redo" size={ 48 } />
+				<Gridicon icon="spam" size={ 48 } />
+				<Gridicon icon="strikethrough" size={ 48 } />
+				<Gridicon icon="tablet" size={ 48 } />
+				<Gridicon icon="tag" size={ 48 } />
+				<Gridicon icon="undo" size={ 48 } />
+				<Gridicon icon="user-circle" size={ 48 } />
+				<Gridicon icon="user" size={ 48 } />
+			</div>
+		);
+	}
+} );
