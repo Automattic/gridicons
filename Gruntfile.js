acd9b2913df047ec42c99cde248d4ce05451a5a6
Joen Asmussen <joen@automattic.com>
2016-01-11 10:39:39 +0100

    Grunt: Fix typo in sprite build script.
    The closing head tag wasn't closing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function(grunt) {
 					<link rel="stylesheet" type="text/css" href="gridicons-demo.css" />
 					<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 					<script src="gridicons-demo.js"></script>
-					<head>
+					</head>
 					<body>
 
 					<h1>Gridicons</h1>
