2f0cd47c43308921740b9af221506bc61c3c4f59
Bernhard Reiter <ockham@raz.or.at>
2017-10-24 21:04:39 +0200

    React: Allow v16

--- sources/react/example-footer.jsx
+++ sources/react/example-footer.jsx
@@ -1,4 +1,6 @@
 			</div>
 		);
 	}
-} );
+};
+
+export default Gridicons;
--- sources/react/example-header.jsx
+++ sources/react/example-header.jsx
@@ -2,19 +2,19 @@
 /**
  * External dependencies
  */
-var React = require( 'react' );
+import React, { PureComponent } from 'react';
 
 /**
  * Internal dependencies
  */
 import Gridicon from './index.js';
-module.exports = React.createClass( {
-	displayName: 'Gridicons',
-	handleClick: function( icon ) {
-		var toCopy = '<Gridicon icon="' + icon + '" />';
-		window.prompt( 'Copy component code:', toCopy );
-	},
 
-	render: function() {
+class Gridicons extends PureComponent {
+	handleClick = ( icon ) => {
+		const toCopy = '<Gridicon icon="' + icon + '" />';
+		window.prompt( 'Copy component code:', toCopy );
+	};
+
+	render() {
 		return (
 			<div>
