2d0a17692d2a07a3ea02ecf87f622f8d9a76bbe4
VassilisDimitrakis <vd@therocket.gr>
2023-01-27 11:47:44 +0200

    Add icon for blaze in gridicons (#323)
    * Add icon for blaze in gridicons

--- php/gridicons.php
+++ php/gridicons.php
@@ -211,6 +211,9 @@ function get_gridicon( $gridicon ) {
 		case 'gridicons-filter':
 			$svg = '<svg class="gridicon gridicons-filter" height="24" width="24" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g><path d="M10 19h4v-2h-4v2zm-4-6h12v-2H6v2zM3 5v2h18V5H3z"/></g></svg>';
 			break;
+		case 'gridicons-fire':
+			$svg = '<svg class="gridicon gridicons-fire" height="24" width="24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g><path d="M21.5 13.4c0 1.2-.2 2.3-.7 3.4-.5 1.1-1.2 2-2.1 2.9-.9.8-1.9 1.5-3.1 1.9-.5.2-1.1.4-1.7.5l.3-.1s2.2-1.3 2.7-4.1c.2-2.5-1.9-3.6-1.9-3.6s-1.1 1.4-2.6 1.4c-2.4 0-2-4-2-4s-3.6 1.9-3.6 5.9c0 2.5 2.6 4.4 2.6 4.4-.3-.1-.7-.2-1-.3-1.2-.4-2.2-1.1-3.1-1.9-.9-.8-1.6-1.8-2.1-2.9-.5-1.1-.7-2.2-.7-3.4 0-3.9 4.6-7.7 4.6-7.7s.4 3 2.7 3c4.2 0 2.7-6.7 2.7-6.7s3.8 2.2 4.9 8.2c2.1-.3 1.9-3 1.9-3s2.2 2.9 2.2 6.1"/></g></svg>';
+			break;
 		case 'gridicons-flag':
 			$svg = '<svg class="gridicon gridicons-flag" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g><path d="M15 6a2 2 0 00-2-2H5v17h2v-7h5a2 2 0 002 2h6V6h-5z"/></g></svg>';
 			break;
